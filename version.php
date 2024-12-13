e33b2035f117cd4e07644f81c2d1299fd5c65f18
ahmedbelhaj <ahmedbelhaj@ahmed.belhaj>
2024-12-13 14:32:37 +0100

    moodle 4.5.1

--- version.php
+++ version.php
@@ -18,5 +18,5 @@ $plugin->component = 'mod_matrix';
 $plugin->dependencies = [];
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '1.0-beta';
-$plugin->requires = 2020061505; // Requires Moodle v3.9 (LTS)
+$plugin->requires = 2024100701; // Requires Moodle v3.9 (LTS)
 $plugin->version = 2022110400; // Plugin released on 4 November 2022
